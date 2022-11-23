88e61aa75c0a2502f1ef322bc19f84d0f68eda77
Kubat Abdrakhmanov <kubatabdrakhmanov@gmail.com>
2022-11-23 22:01:27 +0600

    Fix code style in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,52 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.jsx",
-    output: {
-        path: path.join(__dirname, "/dist"), // the bundle output path
-        filename: "bundle.js", // the name of the bundle
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/dist'), // the bundle output path
+    filename: 'bundle.js', // the name of the bundle
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html', // to import index.html file inside index.js
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, '/'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "index.html", // to import index.html file inside index.js
-        }),
-    ],
-    devServer: {
-        historyApiFallback: true,
-        static: {
-            directory: path.join(__dirname, "/"),
+    port: 3030,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
         },
-        port: 3030,
-        open: true
-   },
-    module:{
-        rules:[
-            {
-                test: /\.(js|jsx)$/, // .js and .jsx files
-                exclude: /node_modules/, // excluding the node_modules folder
-                loader: "babel-loader",
-                options:{
-                    presets:[ "@babel/preset-react"]
-                }
-              },
-              {
-                test: /\.css$/,
-                use: [
-                  'style-loader',
-                  'css-loader'
-                ]
-            },
-            {
-                test: /\.(gif|png|jpe?g)$/,
-                use: [
-                  'file-loader',
-                ]
-              },
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-}
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
